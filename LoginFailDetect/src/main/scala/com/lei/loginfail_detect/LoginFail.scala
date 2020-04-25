c1f8424feaec5859995b91e90508139ececb61ad
leizuquan1990 <843291011@qq.com>
2020-04-25 10:38:49 +0800

    # 新增 电商用户行为分析     恶意登录监控

---
+++ LoginFailDetect/src/main/scala/com/lei/loginfail_detect/LoginFail.scala
@@ -0,0 +1,107 @@
+package com.lei.loginfail_detect
+
+import java.net.URL
+import java.util
+
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ * @Author: Lei
+ * @E-mail: 843291011@qq.com
+ * @Date: Created in 10:04 上午 2020/4/25
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+
+/**
+ * 电商用户行为分析     恶意登录监控
+ *
+ * 用户在2分钟内连续2次登陆失败，判定为异常登陆
+ *
+ */
+
+// 输入的登陆事件样例类
+case class LoginEvent(userId:Long, ip:String, eventType:String, eventTime:Long)
+// 输出的异常报警信息样例类
+case class Warning(userId:Long, firstFailTime:Long, lastFailTime:Long, warningMsg:String)
+
+object LoginFail {
+  def main(args: Array[String]): Unit = {
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.setParallelism(1)
+
+    val resource: URL = getClass.getResource("/LoginLog.csv")
+
+    val loginEventStream: DataStream[LoginEvent] = env.readTextFile(resource.getPath)
+      .map(data => {
+        val dataArray: Array[String] = data.split(",")
+        LoginEvent(dataArray(0).trim.toLong, dataArray(1).trim, dataArray(2).trim, dataArray(3).trim.toLong)
+      })
+      // 如果数据是乱序，则需要进行watermark处理
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[LoginEvent](Time.seconds(5)) {
+        override def extractTimestamp(element: LoginEvent): Long = element.eventTime * 1000L
+      })
+
+    val dataStream: DataStream[Warning] = loginEventStream
+      .keyBy(_.userId) // 以用户id做分组
+      .process(new LoginWarning(2))
+
+
+    dataStream.print()
+
+    env.execute("login fail detect job")
+  }
+}
+
+// 状态编程
+class LoginWarning(maxFailTimes: Int) extends KeyedProcessFunction[Long, LoginEvent, Warning]{
+  // 定义状态，保存2秒内的所有登录失败事件
+  lazy val loginFailState: ListState[LoginEvent] = getRuntimeContext.getListState(new ListStateDescriptor[LoginEvent]("login-fail-state", classOf[LoginEvent]))
+
+
+  override def processElement(value: LoginEvent, ctx: KeyedProcessFunction[Long, LoginEvent, Warning]#Context, collector: Collector[Warning]): Unit = {
+    val loginFailList = loginFailState.get()
+
+    // 判断类型是否fail，只添加fail的事件到状态
+    if (value.eventType == "fail"){
+      if (!loginFailList.iterator().hasNext){ // 如果之前没有数据，需要注册定时器
+        ctx.timerService().registerEventTimeTimer(value.eventTime * 1000L + 2000L)
+      }
+
+      loginFailState.add(value)
+    } else {
+      // 如果是成功，清空状态
+      loginFailState.clear()
+    }
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, LoginEvent, Warning]#OnTimerContext, out: Collector[Warning]): Unit = {
+    // 触发定时器的时候，根据状态里的失败个数决定是否输出报警
+    val allLoginFails = new ListBuffer[LoginEvent]
+    val iter: util.Iterator[LoginEvent] = loginFailState.get.iterator()
+    while (iter.hasNext) {
+      allLoginFails += iter.next()
+    }
+
+    // 判断个数
+    if (allLoginFails.length >= maxFailTimes){
+     // out.collect(Warning(ctx.getCurrentKey, ))
+      out.collect(Warning(allLoginFails.head.userId, allLoginFails.head.eventTime, allLoginFails.last.eventTime, "login fail in 2 seconds for " + allLoginFails.length + " times"))
+    }
+
+    // 报警完后，将状态清空
+    loginFailState.clear()
+
+  }
+}
